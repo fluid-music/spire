7d3d837b848cec84be5900e109fc7d54f98ce58f
Charles Holbrow <CharlesHolbrow@gmail.com>
2021-06-20 02:28:39 -0400

    detailed choices for many params

--- gen-spire-vst2.js
+++ gen-spire-vst2.js
@@ -1,5 +1,7 @@
-const tCompReport = require('./vst2-report')
+
 const fluid = require('fluid-music')
+const spireReport = require('./vst2-report')
+const detailParamReport = require('./vst2-param-report')
 
 function createChoices(choices) {
   const result = {}
@@ -7,8 +9,11 @@ function createChoices(choices) {
   return result
 }
 
-for (const paramInfo of tCompReport.params) {
-  if (paramInfo.name.endsWith('_uoct')) {
+for (const paramInfo of spireReport.params) {
+  if (detailParamReport.hasOwnProperty(paramInfo.name)) {
+    paramInfo.choices = createChoices(detailParamReport[paramInfo.name])
+  }
+  else if (paramInfo.name.endsWith('_uoct')) {
     paramInfo.key = `osc${paramInfo.name[3]}UnisonInterval`
     paramInfo.choices = createChoices([ '8va', '16va', '24va', 'maj3rd', 'min3rd', 'maj7th', 'dom7th', 'minmaj7th', 'minor7th', 'min9th', 'maj9th', 'dom9th', 'halfdim7th', 'dim7th', 'sus2', 'sus4', 'p5th', 'p4th'])
   }
@@ -22,7 +27,7 @@ for (const paramInfo of tCompReport.params) {
   }
 }
 
-const moduleString = fluid.gen.generatePluginModule(tCompReport)
+const moduleString = fluid.gen.generatePluginModule(spireReport)
 const fs = require('fs')
 const path = require('path')
 const filename = path.join(__dirname, 'src', 'spire-vst2.ts')
